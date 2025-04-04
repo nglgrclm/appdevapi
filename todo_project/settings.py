a0c4b865c040cede8c6bb45ca3ffc6474c229f5a
Angel Grace Murillo <100488241+nglgrclm@users.noreply.github.com>
2025-04-04 21:19:44 +0800

    Add files via upload

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'appdevapi.onrender.com']
+ALLOWED_HOSTS = ['https://appdevapi.onrender.com/api/tasks/']
 
 # Application definition
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'todo_app',
 ]
 
@@ -123,6 +124,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'https://appdevapi.onrender.com/api/tasks/'
+    # Your frontend's URL
+]
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
