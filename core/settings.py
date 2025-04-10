8986771b70d52e6470b37327c3f7daafb78a83c6
nglgrclm <murillo.angelgrace@gmail.com>
2025-04-11 07:46:01 +0800

    Add Token model to admin

--- core/settings.py
+++ core/settings.py
@@ -43,10 +43,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 
     'todolist',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
