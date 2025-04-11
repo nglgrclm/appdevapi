30abf4571cb031f0c8a1182717dede25396d03db
nglgrclm <murillo.angelgrace@gmail.com>
2025-04-11 20:47:54 +0800

    Auth Token

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import ToDoList, ToDoDetail
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import SecureHelloView
 
 urlpatterns = [
     path('todos/', ToDoList.as_view(), name='todo-list'),
     path('todos/<int:pk>/', ToDoDetail.as_view(), name='todo-detail'),
+    path('api-token-auth/', obtain_auth_token),
+    path('secure-hello/', SecureHelloView.as_view()),
 ]
